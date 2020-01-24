6b69c6d6b57f13fb161e028c80b9a1f850ff148b
Franciszek Gorczyca <izdnozrkenar@github.com>
2020-01-24 01:36:13 -0800

    add plotting

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from book import Book, Covers, Papers, Fonts
 from books_excel_helper import get_books_csv_data
 from groups_excel_helper import get_groups_csv_data
 from population import Population
+import matplotlib.pyplot as plt
 
 #get data from csv files 
 books_data = get_books_csv_data()
@@ -10,12 +11,16 @@ groups_data = get_groups_csv_data()
 #population size must be at least 2 
 pop = Population(20,groups_data,books_data)
 
-print(pop.best_genotype)
-pop.tournament_crossing(3)
-print(pop.best_genotype)
-pop.mutation(0.3)
-print(pop.best_genotype)
-pop.random_crossing(2)
-print(pop.best_genotype)
-pop.mutation(0.7)
-print(pop.best_genotype)
+iterations = []
+best_fitness_val = []
+
+for i in range(20):
+    iterations.append(i)
+    pop.mutation(0.9)
+    best_fitness_val.append(pop.best_genotype[1])
+
+plt.plot(iterations,best_fitness_val)
+plt.title('20 mutations', size=14)
+plt.xlabel('iterations', size=14)
+plt.ylabel('fitness value', size=14)
+plt.savefig('plots/mutations.png', format='png')
