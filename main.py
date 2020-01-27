3a06dcd397cb3f779db4e173fd80fa0980f9b08e
Franciszek Gorczyca <izdnozrkenar@github.com>
2020-01-26 22:22:26 -0800

    v 1.0

--- main.py
+++ main.py
@@ -11,21 +11,40 @@ groups_data = get_groups_csv_data()
 #population size must be at least 2 
 pop = Population(500,groups_data,books_data)
 
-print(pop.best_genotype)
-
-iterations = []
+its = 10
 best_fitness_val = []
+av_fitness_val = []
+iterations = []
 
-for i in range(20):
+for i in range(3):
+    pop.roulette_selection()
     iterations.append(i)
-    pop.mutation(0.5)
     best_fitness_val.append(pop.best_genotype[1])
+    av_fitness_val.append(pop.get_average_fitness_value())
 
 
-print(pop.best_genotype)
+for i in range(its):
+    iterations.append(i+3)
+    pop.tournament_crossing(40)
+    best_fitness_val.append(pop.best_genotype[1])
+    av_fitness_val.append(pop.get_average_fitness_value())
 
-plt.plot(iterations,best_fitness_val)
-plt.title('20 mutations', size=14)
-plt.xlabel('iterations', size=14)
-plt.ylabel('fitness value', size=14)
+for i in range(3):
+    pop.roulette_selection()
+    iterations.append(i + 13)
+    best_fitness_val.append(pop.best_genotype[1])
+    av_fitness_val.append(pop.get_average_fitness_value())
+
+for i in range(its):
+    iterations.append(i+16)
+    pop.tournament_crossing(4)
+    best_fitness_val.append(pop.best_genotype[1])
+    av_fitness_val.append(pop.get_average_fitness_value())
+
+plt.step(iterations,best_fitness_val)
+plt.step(iterations,av_fitness_val)
+plt.title('Preselection and tournament crossing 20 groups', size=15)
+plt.xlabel('iterations', size=15)
+plt.ylabel('fitness value', size=15)
 plt.savefig('plots/mutations.png', format='png')
+
